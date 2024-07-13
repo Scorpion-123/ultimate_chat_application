7f3a1d98f5818a371cd9d66e125562dcb69eab5e
Ankit Dey <ankitdey153@gmail.com>
2024-07-14 02:59:22 +0530

    import errors

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
+import os, django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -15,7 +18,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from a_rtchat import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
 application = ProtocolTypeRouter({
     'http' : get_asgi_application(),
